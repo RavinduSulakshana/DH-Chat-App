1179779460c321ed32e1719bba613ab71690aade
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-07-12 07:06:27 +0530

    added User management

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"chatapp/handlers"
+	"chat-app-backend/handlers"
+	"chat-app-backend/models"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var Client = make(map[*websocket.conn]*models.User)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 func main() {
 	http.HandleFunc("/ws", handlers.WebSocketHandler)
 	fmt.Println("Server starting on :8080...")
-	http.ListenAndServer(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
