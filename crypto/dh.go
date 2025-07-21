37b32b9d380066fb9f65c7188afe16f471dd6ce5
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-07-22 00:35:02 +0530

    add some changes

--- crypto/dh.go
+++ crypto/dh.go
@@ -1,45 +1,19 @@
 package crypto
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/sha256"
+	"math/big"
 )
 
-type DHKeyPair struct {
-	PrivateKey *ecdsa.PrivateKey
-	PublicKey  *ecdsa.PublicKey
-}
+var p, _ = new(big.Int).SetString("23", 10)
+var g = big.NewInt(5)
 
-func GenerateDHKeyPair() (*DHKeyPair, error) {
-	curve := elliptic.P256()
-	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+func GenerateKeyPair() (*big.Int, *big.Int, error) {
+	private, err := rand.Int(rand.Reader, new(big.Int).Sub(p, big.NewInt(2)))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-
-	return &DHKeyPair{
-		PrivateKey: privateKey,
-		PublicKey:  &privateKey.PublicKey,
-	}, nil
-}
-
-// compute shared secret key using local private key and remote private key
-func ComputeSharedSecret(privateKey *ecdsa.PrivateKey, remotePubKey *ecdsa.PublicKey) ([]byte, error) {
-	x, _ := elliptic.P256().ScalarMult(remotePubKey.X, remotePubKey.Y, privateKey.D.Bytes())
-	sharedSecret := sha256.Sum256(x.Bytes())
-	return sharedSecret[:], nil
-}
-
-// Encode publicKey to Bytes for transmission
-func EncodePublicKey(pubKey *ecdsa.PublicKey) []bytes {
-	return elliptic.Marshal(elliptic.P256(), pubKey.X, pubKey.Y)
-}
-
-// Decode bytes to public key
-func DecodePublicKey(data []bytes) *ecdsa.PublicKey {
-	x, y := elliptic.Unmarshal(elliptic.P256(), data)
-	return &ecdsa.PublicKey{Curve: elliptic.P224(), X: x, Y: y}
-
+	private.Add(private, big.NewInt(2))
+	public := new(big.Int).Exp(g, private, p)
+	return private, public, nil
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var Client = make(map[*websocket.conn]*models.User)
+var Client = make(map[*websocket.Conn]*models.User)
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
