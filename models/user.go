fe3dd940019f8ac6c4d0670c5839dab96356756c
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-07-30 08:26:56 +0530

    user model implemented

--- models/user.go
+++ models/user.go
@@ -1,5 +1,27 @@
 package models
 
+import (
+	"math/big"
+
+	"github.com/gorilla/websocket"
+)
+
 type User struct {
-	Username string
+	ID            string              `json:"id"`
+	UserName      string              `json:"username"`
+	Conn          *websocket.Conn     `json:"-"`
+	PublicKey     *big.Int            `json:"public_key,omitempty"`
+	PrivateKey    *big.Int            `json:"-"`
+	SharedSecrets map[string]*big.Int `json:"-"`
+	AESKeys       map[string][]byte   `json:"-"`
+}
+
+func NewUser(id, username string, conn *websocket.Conn) *User {
+	return &User{
+		ID:            id,
+		Username:      username,
+		Conn:          conn,
+		SharedSecrets: make(map[string]*big.Int),
+		AESKeys:       make(map[string][]byte),
+	}
 }
